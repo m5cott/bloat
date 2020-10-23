c626efe8929fb4e0e482cf6c83f6f9eeb67c06f0
m5cott <mcscottbsn@outlook.com>
2020-10-22 20:28:48 -0500

    added check for windows only functions

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,8 @@
-# Elevating privileges in Powershell 7
-if ($isLinux)
-{
-    function Get-Sudo { & /usr/bin/env sudo pwsh -command "& $args" }
-    set-alias sudo Get-Sudo
-}
+##
+# Name: Microsoft.PowerShell_profile.ps1
+# Author: Michael Scott (m5cott)
+# Created: 2020-10-18
+##
 
 # Importing posh-git and oh-my-posh modules
 Import-Module posh-git
@@ -22,8 +21,17 @@ Set-Alias nd New-ItemDirectory
 function New-ItemFile { New-Item -Type File -Name "$args"}
 Set-Alias nf New-ItemFile
 
-function shasum { certutil.exe -hashfile "$args" SHA256 }
-Set-Alias sha256 shasum
+# Elevating privileges in Powershell 7
+if ($isLinux)
+{
+    function Get-Sudo { & /usr/bin/env sudo pwsh -command "& $args" }
+    set-alias sudo Get-Sudo
+}
 
-function md5sum { certutil.exe -hashfile "$args" MD5 }
-Set-Alias md5 md5sum
+if ($isWindows) {
+    function shasum { certutil.exe -hashfile "$args" SHA256 }
+    Set-Alias sha256 shasum
+    
+    function md5sum { certutil.exe -hashfile "$args" MD5 }
+    Set-Alias md5 md5sum
+}
